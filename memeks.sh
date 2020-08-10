a0dea8fba1abc6dc5fd4897eb852ff34b2189e3a
Security Vx <68503915+BangArex@users.noreply.github.com>
2020-08-10 22:19:24 +0700

    Add files via upload

---
+++ memeks.sh
@@ -0,0 +1,144 @@
+#!/bin/sh
+
+clear
+sleep 0.30
+
+echo "___ ____ ____ _    ____    _  _ ____ _  _ _  _ \033[36;1m_  _ 
+ \033[31;1m|  |  | |  | |    [__     |\ | |___ \033[31;1m|\/| \033[37;1m|  | |  | 
+ \033[31;1m|  |__| |__| |___ ___]    | \| |___ \033[31;1m|  | \033[0;37m|__|  \/  
+                                                    
+"
+echo "\033[36;1m╔══════════════════════════════════════╗"
+echo "\033[36;1m║  \033[33;1m*  \033[37;1mAuthor   \033[0;37m:   \033[0;32mNemuv               \033[36;1m║"
+echo "\033[36;1m║  \033[33;1m*  \033[37;1mTeam     \033[0;37m:   \033[37;1mCroco\033[0;33mID   \033[36;1m          ║"
+echo "\033[36;1m║  \033[0;31m-  \033[37;1mGithub   \033[0;37m:   \033[37;1mGithub.com/BangArex \033[36;1m║"
+echo "\033[36;1m╚═══════════║═════════║════════════════╝"
+echo "\033[36;1m            ║[\033[41m \033[37;1mTOOLS \033[00m\033[1;94m\033[36;1m]║ "
+echo "\033[36;1m            ╚═════════╝ "
+echo ""
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m1.\033[33;1m] \033[0;37m>  \033[0;36mDarkfb "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m2.\033[33;1m] \033[0;37m>  \033[0;36mSpamSms "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m3.\033[33;1m] \033[0;37m>  \033[0;36mButton "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m4.\033[33;1m] \033[0;37m>  \033[0;36mTermux-Style "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m5.\033[33;1m] \033[0;37m>  \033[0;36mSpamCall  \033[32;1m[\033[31;1mClosed\033[32;1m]"
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m6.\033[33;1m] \033[0;37m>  \033[0;36mDeface Maker "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m7.\033[33;1m] \033[0;37m>  \033[0;36mInstall Metasploit "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m8.\033[33;1m] \033[0;37m>  \033[0;36mDeface Web "
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m9.\033[33;1m] \033[0;37m>  \033[0;36mDecrypt And Encrypt Bash "
+echo ""
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════\033[33;1m[ \033[36;1m0.\033[33;1m] \033[0;37m>  \033[0;31mKeluar "
+sleep 0.1
+echo ""
+sleep 0.1
+echo "\033[31;1m-\033[36;1m-----------------------------------------\033[31;1m-"
+sleep 0.1
+echo "\033[31;1m|\033[36;1m════00. \033[0;37m> \033[36;1m[\033[41m \033[37;1mSubscribe Channel [ Decrypt Bot Sc ™ \033[00m\033[1;94m\033[36;1m] "
+echo ""
+sleep 0.1
+read -p "#*Pilih > " pill;
+#Batas Gan
+if [ $pill = 1 ]
+then
+git clone https://github.com/TheMagizz/DarkPremium
+pip2 install mechanize
+pip2 install requests
+pip2 install -r requirements.txt
+cd DarkPremium
+python2 DarkFB.py
+
+fi
+#Batas Gan
+if [ $pill = 2 ]
+then
+git clone https://github.com/BangArex/spam-sms_otp-pizzaHut
+pip2 install mechanize
+pip2 install requests
+pkg install php
+cd spam-sms_otp-pizzaHut
+php sc_telp.php
+
+fi
+#Batas Gan
+if [ $pill = 3 ]
+then
+git clone https://github.com/BangArex/Button---Termux---Creator
+pip2 install mechanize
+pip2 install requests
+cd Button---Termux---Creator
+python2 button.py
+
+fi
+#Batas Gan
+if [ $pill = 4 ]
+then
+git clone https://github.com/B4N954N2-ID/termux-style
+cd termux-style
+python2 run.py
+
+fi
+#Batas Gan
+if [ $pill = 6 ]
+then
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+
+fi
+#Batas Gan
+if [ $pill = 7 ]
+then
+apt update && apt upgrade
+apt install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+
+fi
+#Batas Gan
+if [ $pill = 8 ]
+then
+pkg install fish
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+apt install curl
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash install.sh
+sleep 1
+bash webdav77.sh
+
+fi
+#Batas Gan
+if [ $pill = 9 ]
+then
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/BangArex/bash-encryptsh
+cd bash-encryptsh
+python2 obdec_enc.py
+
+fi
+#Batas Gan
+if [ $pill = 0 ]
+then
+exit
+fi
+#Batas Gan
+if [ $pill = 00 ]
+then
+xdg-open https://www.youtube.com/channel/UCHVjMNRJELL7smMlvWr3LzQ
+
+fi
+#Batas Gan
